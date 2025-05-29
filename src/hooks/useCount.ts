d55bf179c002f8636d1d64da2cb98a929547419c
Edward205204 <khoa123kd56cn@gmail.com>
2025-05-29 21:42:23 +0700

    chore: setup auto public npm

--- src/hooks/useCount.ts
+++ src/hooks/useCount.ts
@@ -8,5 +8,7 @@ export function useCount() {
     increment: () => setCount(count + 1),
     decrement: () => setCount(count - 1),
     reset: () => setCount(0),
+    double: () => setCount(count * 2),
+    half: () => setCount(count / 2),
   };
 }
